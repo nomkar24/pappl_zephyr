83d499116667aac6c3cd00b48962e3780e9c1416
Omkar Nanajkar <nomkar2131@gmail.com>
2026-06-04 17:58:26 +0530

    added Kconfig and config.h file to solve dependency issues

--- config.h
+++ config.h
@@ -5,6 +5,10 @@
 #ifndef _PAPPL_CONFIG_H_
 #define _PAPPL_CONFIG_H_
 
+#include <stddef.h>
+#include <stdio.h>
+#include <cups/cups.h>
+
 // Version numbers (matching PAPPL 2.0)
 #define PAPPL_VERSION "2.0.0"
 #define PAPPL_VERSION_MAJOR 2
@@ -44,15 +48,24 @@
 #undef HAVE_PDFIO
 
 // POSIX/CUPS Shims for Zephyr
-#define realpath(x, y) y
-#define popen(x, y) NULL
-#define pclose(x) 0
-#define cupsAddDest(x,y,z,w) 0
-#define cupsGetDest(x,y,z,w) NULL
+#define realpath(x, y) (y)
+
+static inline FILE *pappl_stub_popen(const char *c, const char *t) { return NULL; }
+#define popen pappl_stub_popen
+
+static inline int pappl_stub_pclose(FILE *f) { return 0; }
+#define pclose pappl_stub_pclose
+
+static inline size_t pappl_stub_cupsAddDest(const char *name, const char *instance, size_t num_dests, cups_dest_t **dests) { return 0; }
+#define cupsAddDest pappl_stub_cupsAddDest
+
+static inline cups_dest_t *pappl_stub_cupsGetDest(const char *name, const char *instance, size_t num_dests, cups_dest_t *dests) { return NULL; }
+#define cupsGetDest pappl_stub_cupsGetDest
 
 // Missing mkstemp on some Zephyr versions
 #ifndef HAVE_MKSTEMP
-#  define mkstemp(t) -1
+static inline int pappl_stub_mkstemp(char *template) { return -1; }
+#  define mkstemp pappl_stub_mkstemp
 #endif
 
 #endif // _PAPPL_CONFIG_H_
